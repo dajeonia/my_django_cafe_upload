dd63cea1c9077c11cae0111e39714750d720ec7a
Ubuntu <ubuntu@ip-172-31-19-183.ap-northeast-2.compute.internal>
2024-06-07 11:21:19 +0900

    get_naver_search: 탭 제거

--- uploadCafeSetting/uploadCafeSetting/get_naver_search.py
+++ uploadCafeSetting/uploadCafeSetting/get_naver_search.py
@@ -4,56 +4,56 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 def get_html(driver):
-	driver.get("https://www.naver.com")
+    driver.get("https://www.naver.com")
 
-	search_box = driver.find_element(By.XPATH, '//*[@id="query"]')
+    search_box = driver.find_element(By.XPATH, '//*[@id="query"]')
 
-	search_box.send_keys("SAP 컨설턴트 채용")
-	search_box.send_keys(Keys.RETURN)
+    search_box.send_keys("SAP 컨설턴트 채용")
+    search_box.send_keys(Keys.RETURN)
 
 	driver.find_element(By.XPATH, '//*[@id="main_pack"]/section[1]/div/div[6]/a').click()
 	driver.find_element(By.XPATH, '//*[@id="content"]/div[1]/section/div[1]/div/div[2]/a[3]').click()
 	sleep(3)
 
-	for i in range(10):
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		sleep(0.5)
-		
-	html = driver.page_source
-	return html
+    for i in range(10):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(0.5)
+        
+    html = driver.page_source
+    return html
 
 from bs4 import BeautifulSoup
 
 def get_recruit_from_naver(html):
-	soup = BeautifulSoup(html, 'html.parser')
-	divs = soup.find_all('div', 'recruit_wrap')
-	list = []
-	for d in divs:
-		recruit = {}
-		recruit['title'] = d.find('a', 'title').text
-		recruit['subtitle'] = d.find('div', 'sub_title').text
-		recruit['tags'] = []
-		recruit['keywords'] = []
-		recruit['link'] = d.find('a', 'link_site')['href']
-		tags = d.find_all('span', 'item')
-		keywords = d.find_all('a', 'link_keyword')
-		for tag in tags:
-			recruit['tags'].append(tag.text)
-		for keyword in keywords:
-			recruit['keywords'].append(keyword.text)
-		list.append(recruit)
-	return (list)
+    soup = BeautifulSoup(html, 'html.parser')
+    divs = soup.find_all('div', 'recruit_wrap')
+    recruit_list = []
+    for d in divs:
+        recruit = {}
+        recruit['title'] = d.find('a', 'title').text
+        recruit['subtitle'] = d.find('div', 'sub_title').text
+        recruit['tags'] = []
+        recruit['keywords'] = []
+        recruit['link'] = d.find('a', 'link_site')['href']
+        tags = d.find_all('span', 'item')
+        keywords = d.find_all('a', 'link_keyword')
+        for tag in tags:
+            recruit['tags'].append(tag.text)
+        for keyword in keywords:
+            recruit['keywords'].append(keyword.text)
+        recruit_list.append(recruit)
+    return (recruit_list)
 
 from datetime import datetime
 
-def make_article(list):
-	article = {}
-	now = datetime.now()
-	article['subject'] = now.strftime("%Y년%m월%d일 %H시 SAP 채용공고")
-	concat = ""
-# article['content'] = ""
-	for content in list:
-		concat += f"""
+def make_article(size, recruit_list):
+    recruit_list = recruit_list[:size]
+    article = {}
+    now = datetime.now()
+    article['subject'] = now.strftime("[SAP 컨설턴트 및 ABAP 채용공고] %Y년%m월%d일 %H시(30건)")
+    concat = ""
+    for content in recruit_list:
+        concat += f"""
 제목: {content['title']}
 부제: {content['subtitle']}
 태그
@@ -68,8 +68,8 @@ def make_article(list):
 	return (article)
 
 
-def get_naver_search(driver):
-	html = get_html(driver)
-	list = get_recruit_from_naver(html)
-	article = make_article(list)
-	return (article)
+def get_naver_search(size, driver):
+    html = get_html(driver)
+    recruit_list = get_recruit_from_naver(html)
+    article = make_article(size, recruit_list)
+    return (article)
